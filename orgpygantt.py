c35b8862f2f7148c8b28c291fa29a93446041fa6
lagrange0125@gmail.com <lagrange0125@gmail.com>
2019-10-02 19:43:50 -0400

    added default commandline function

--- orgpygantt.py
+++ orgpygantt.py
@@ -7,160 +7,7 @@ from orgparse import load, loads, node
 plan = []
 nodeid=''
 verbose = False
-
-#orgfile = '/home/lcommons/whereisthis/lcommons/org-files/house2.org'
-#root = load('/home/lcommons/whereisthis/lcommons/org-files/house2.org')
-orgtree='''#+TITLE: The Plan for Building Our House
-#+HTML_HEAD: <link rel="stylesheet" type="text/css" href="style/style1.css" />
-#+HTML_HEAD_EXTRA: <link rel="alternate stylesheet" type="text/css" href="style2.css" />
-
-* House build plan
-  SCHEDULED: <2020-09-16 Wed>
-  :PROPERTIES:
-  :ID:       house-plan
-  :ORDERED:  t
-  :END:      
-** Site prep
-  :PROPERTIES:
-  :END:      
-*** Level house site
-    :PROPERTIES:
-    :Effort:   16:00
-    :END:      
-*** drainage
-    :PROPERTIES:
-    :Effort:   16:00
-    :END:      
-*** septic system
-    :PROPERTIES:
-    :Effort:   16:00
-    :END:      
-*** Excavate for foundation 
-    :PROPERTIES:
-    :Effort:   40:00
-    :END:      
-** Foundation
-  :PROPERTIES:
-  :END:      
-*** Waste Water drain pipe install     
-   :PROPERTIES:
-   :Effort:   48:00
-   :END:
-**** Layout exact location of each drain
-   :PROPERTIES:
-   :Effort:   8:00
-   :END:
-**** trench for waste water drains
-   :PROPERTIES:
-   :Effort:   8:00
-   :END:
-**** gravel in waste water drain trenchs
-   :PROPERTIES:
-   :Effort:   8:00
-   :END:
-**** install waste water drains
-   :PROPERTIES:
-   :Effort:   16:00
-   :END:
-   :END:
-*** add additional gravel 
-   More information about the [[file:slab.org][how the slab is built]].
-   :PROPERTIES:
-   :Effort:   8:00
-   :END:
-*** Add insulation 
-   More information about the [[file:slab.org][how the slab is built]].
-   :PROPERTIES:
-   :Effort:   8:00
-   :END:
-*** rebar, wire mesh
-   :PROPERTIES:
-   :Effort:   24:00
-   :END:
-*** Radiant Heat
-Read more about the [[file:radiant-heat.org][radiant heat plan]]
-   :PROPERTIES:
-   :Effort:   16:00
-   :END:
-*** Build Forms
-   More information about how the forms will be built [[file:slab.org][here]].
-   :PROPERTIES:
-   :Effort:   40:00
-   :END:
-*** pour slab
-   :PROPERTIES:
-   :Effort:   8:00
-   :END:
-** Cut timbers
-   :PROPERTIES:
-   :Effort:   160:00
-   :END:
-** Assemble bents 
-   :PROPERTIES:
-   :Effort:   40:00
-   :END:
-** Raise Frame
-   :PROPERTIES:
-   :Effort:   40:00
-   :END:
-** Roof
-   More information about the [[file:roof.org][roof]].
-   :PROPERTIES:
-   :Effort:   80:00
-   :END:      
-** Walls
-   More information about the plan for the [[file:walls.org][walls]].
-   :PROPERTIES:
-   :Effort:   160:00
-   :END:
-** Interior
-*** Utility room
-   More information about the plan for the [[file:utilityroom.org][utility room]].
-   :PROPERTIES:
-   :Effort:   80:00
-   :END:
-*** Laundry room
-   :PROPERTIES:
-   :Effort:   40:00
-   :END:
-*** Mud room
-   More information about the plan for the [[file:mudroom.org][mud room]].
-   :PROPERTIES:
-   :Effort:   24:00
-   :END:
-*** Kitchen
-   More information about the plan for the [[file:kitchen.org][kitchen]].
-   :PROPERTIES:
-   :Effort:   120:00
-   :END:
-*** bathrooms
-   More information about the plan for the [[file:bathrooms.org][bathrooms]].
-   :PROPERTIES:
-   :Effort:   40:00
-   :END:
-*** living room
-   More information about the plan for the [[file:livingroom.org][living room]].
-   :PROPERTIES:
-   :Effort:   40:00
-   :END:
-*** master bedroom
-   More information about the plan for the [[file:masterbedroom.org][master bedroom]].
-   :PROPERTIES:
-   :Effort:   40:00
-   :END:
-*** other bedrooms
-   More information about the plan for the [[file:bedrooms.org][bedrooms]].
-   :PROPERTIES:
-   :Effort:   40:00
-   :END:
-** move in!  :milestone:
-    :PROPERTIES:
-    :ID: movein
-    :END:
-'''
-
-
-
+output = ''
 one_day = datetime.timedelta(1)
 
 def set_dates_in_parent_tasks(node_list,plan):
@@ -283,35 +130,45 @@ class Task:
 
     return dict
 
-helptext = "This program converts an org-mode file to a gantt chart. Usage: -f filename"
-#parser = argparse.ArgumentParser()
-parser = argparse.ArgumentParser(description = helptext)
-parser.add_argument('-f', help='The name of the org file to be processed')
-parser.add_argument('-id', help='the id of the root node to be processed, if the org file contains additional nodes other than the project plan.')
-parser.add_argument('-v', action='store_true', help='print each node as it is processed.')
-args = parser.parse_args()
-print('-f',args.f)
-print('-id',args.id)
-print('-v',args.v)
+def ganttify(filename, output='gantt.png'):
+    root = load(filename)    
+    set_dates_in_parent_tasks(root[1:],plan)
+    plan.reverse()
+
+    colors = {'summary': 'rgb(0, 0, 0)',
+              'detail': 'rgb(50, 70, 238)'}
+    fig = ff.create_gantt(plan,colors=colors, index_col='Type')
+    #fig.show()
+    fig.layout.title="Build the dream home... or the Home build dream"
+    fig.layout.yaxis['tickfont'] = {'family': 'Courier New', 'size': 8}
+    fig.layout.xaxis['rangeselector']={}
+    
+    fig.layout.yaxis['showgrid']=True
+    fig.layout.xaxis['showgrid']=True
+
+    fig.write_image(output)
 
 
-#root = loads(orgtree)
-#filename = "../../org-files/house2.org"
-root = load(args.f)
-set_dates_in_parent_tasks(root[1:],plan)
-verbose = args.v
-plan.reverse()
-#print(plan)
-colors = {'summary': 'rgb(0, 0, 0)',
-          'detail': 'rgb(50, 70, 238)'}
-fig = ff.create_gantt(plan,colors=colors, index_col='Type')
-#fig.show()
-fig.layout.title="Build the dream home... or the Home build dream"
-fig.layout.yaxis['tickfont'] = {'family': 'Courier New', 'size': 8}
-fig.layout.xaxis['rangeselector']={}
+'''
+if running at the command line, parse the args 
+and pass them to ganttify
 
-fig.layout.yaxis['showgrid']=True
-fig.layout.xaxis['showgrid']=True
-
-fig.write_image("fig1.png")
-
+'''
+if __name__ == "__main__":
+    
+    helptext = "This program tries to create a gantt chart image file from an org-mode file. Usage: -f input org-mode filename -g output image file name"
+    #parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description = helptext)
+    parser.add_argument('-f', help='The name of the org file to be processed')
+    parser.add_argument('-g', help='The name of the gantt chart image file to produce', default="gantt.png")
+    parser.add_argument('-id', help='the id of the root node to be processed, if the org file contains additional nodes other than the project plan.')
+    parser.add_argument('-v', action='store_true', help='print each node as it is processed.')
+    args = parser.parse_args()
+    filename = args.f
+    verbose = args.v
+    output = args.g
+    
+    print('-f',args.f)
+    print('-id',args.id)
+    print('-v',args.v)
+    ganttify(filename,output)
